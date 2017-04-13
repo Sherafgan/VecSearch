d70c2b4db769cf5d4bd520c8e82308e42243c02c
Sherafgan <kssfortis@gmail.com>
2017-04-13 10:56:17 +0300

    fixed issue with java heap space

---
+++ src/main/java/indexing/IO.java
@@ -0,0 +1,38 @@
+package indexing;
+
+import javafx.util.Pair;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author Sherafgan Kandov (sherafgan.kandov@gmail.com)
+ * @version 4/13/17
+ */
+public class IO {
+    private final FileReader fileReader;
+    private final BufferedReader bufferedReader;
+
+    public IO(String fileName) throws FileNotFoundException {
+        this.fileReader = new FileReader(fileName);
+        this.bufferedReader = new BufferedReader(fileReader);
+    }
+
+    public Pair<String, String> readNextPair() throws IOException {
+        Pair<String, String> idAndVector;
+        String id = bufferedReader.readLine();
+        String vector = bufferedReader.readLine();
+        if (id != null && vector != null) {
+            idAndVector = new Pair<>(id, vector);
+        } else {
+            throw new IOException();
+        }
+        return idAndVector;
+    }
+
+    public void close() throws IOException {
+        bufferedReader.close();
+    }
+}
--- src/main/java/indexing/Indexer.java
+++ src/main/java/indexing/Indexer.java
@@ -1,50 +1,38 @@
 package indexing;
 
+import javafx.util.Pair;
 import vectorization.client.Client;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
- * @author  Sherafgan Kandov (sherafgan.kandov@gmail.com)
+ * @author Sherafgan Kandov (sherafgan.kandov@gmail.com)
  * @version 3/18/17
  */
 public class Indexer {
     private static final int TOTAL_DESCRIPTIONS_PER_VIDEO = 20;
-    private static final int DESCRIPTIONS_PER_VIDEO = 5; // 5/19 Index/Test split
+    private static final int DESCRIPTIONS_PER_VIDEO = 15; // 5/19 Index/Test split
 
     private final static String DB_INDEX_DATA_FILENAME = "tmpFiles/dbIndexData_" + DESCRIPTIONS_PER_VIDEO
             + "-" + (TOTAL_DESCRIPTIONS_PER_VIDEO - DESCRIPTIONS_PER_VIDEO) + "_split" + ".txt";
 
     public static void main(String[] args) throws IOException {
-        List<String> videoIdAndVector = readDbIndexData(DB_INDEX_DATA_FILENAME);
+        IO io = new IO(DB_INDEX_DATA_FILENAME);
         Client client = new Client("VecSearchData", "localhost", 4567);
         client.sendRequest("create space videoSpace with dimensionality 300");
         int counter = 0;
 //        for (int i = 0; i < videoIdAndVector.size(); i += 2) { // ORIGINAL
-        for (int i = 0; i < 100; i += 2) { // DUMMY or i+=29 for unique from 15 duplicate descriptions
-            String request = "insert " + videoIdAndVector.get(i) + "_" + (++counter) + " "
-                    + "=[" + videoIdAndVector.get(i + 1) + "] into videoSpace";
+        for (int i = 0; i < 105; i++) { // indexing 7 videos with 15 annotation per video
+            Pair<String, String> videoIdAndVector = io.readNextPair();
+            String request = "insert " + videoIdAndVector.getKey() + "_" + (++counter) + " "
+                    + "=[" + videoIdAndVector.getValue() + "] into videoSpace";
             if (counter == DESCRIPTIONS_PER_VIDEO) {
                 counter = 0;
             }
             client.sendRequest(request);
-            System.out.println("[INFO] INDEXED: " + (i + 2) / 2);
+            System.out.println("[INFO] INDEXED: " + (i + 1));
         }
+        io.close();
         client.sendRequest("exit");
-    }
-
-    private static List<String> readDbIndexData(String batchFileName) throws IOException {
-        List<String> videoIdAndVector = new LinkedList<>();
-        FileReader fileReader = new FileReader(batchFileName);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            videoIdAndVector.add(line);
-        }
-        return videoIdAndVector;
     }
 }
