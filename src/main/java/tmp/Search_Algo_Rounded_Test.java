d56f0f730c5a34c91bab29a6cd16b7f543b01212
Sherafgan <kssfortis@gmail.com>
2017-04-18 07:19:08 +0300

    updated gitignore

---
+++ src/main/java/tmp/Search_Algo_Rounded_Test.java
@@ -0,0 +1,32 @@
+package tmp;
+
+import db.VectorizationDB;
+import javafx.util.Pair;
+import util.IO;
+
+import java.io.IOException;
+import java.util.Set;
+
+/**
+ * @author Sherafgan Kandov (sherafgan.kandov@gmail.com)
+ * @version 4/17/17
+ */
+public class Search_Algo_Rounded_Test {
+    private static final int TOTAL_DESCRIPTIONS_PER_VIDEO = 20;
+    private static final int DESCRIPTIONS_PER_VIDEO = 20;
+    private static final String TEST_VECTORS_PATH = "tmpFiles/test_rounding_vectors_improved_13x20_dbIndexData_"
+            + DESCRIPTIONS_PER_VIDEO
+            + "-" + (TOTAL_DESCRIPTIONS_PER_VIDEO - DESCRIPTIONS_PER_VIDEO) + "_split" + ".txt";
+
+    public static void main(String[] args) throws IOException {
+        VectorizationDB.open();
+        IO io = new IO(TEST_VECTORS_PATH);
+        for (int i = 0; i < 260; i++) {
+            Pair<String, String> pair = io.readNextPair();
+            System.out.print("[INFO] SEARCH " + (i + 1) + ": ");
+            Set<String> videoIDs = VectorizationDB.retrieveNearestVectorsIds(15, pair.getValue());
+            System.out.println();
+        }
+        VectorizationDB.close();
+    }
+}
