b8e5cbc171202b024e8057d099d07fd1ce33e4e1
Sherafgan <kssfortis@gmail.com>
2017-04-19 11:30:31 +0300

    SearchSim.java refactoring

--- src/main/java/searching/SearchSim.java
+++ src/main/java/searching/SearchSim.java
@@ -1,20 +1,20 @@
 package searching;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Sherafgan Kandov (sherafgan.kandov@gmail.com)
  * @version 4/16/17
  */
 public class SearchSim {
-    public static List<String> search(String searchText) {
+    public static Set<String> search(String searchText) {
         //Vectorize searchText to searchTextVector
         //Search in VectorizationDB for k nearest vectors to searchTextVector
         //Retrieve IDs of nearest vectors
         //Search in SQL DB for detailed information of videos with those IDs
         //return results to frontend
-        List<String> videoIDs = new LinkedList<>();
+        Set<String> videoIDs = new HashSet<>();
         videoIDs.add("video14");
         videoIDs.add("video345");
         videoIDs.add("video1001");
